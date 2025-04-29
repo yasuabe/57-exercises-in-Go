f1a67a4964719fe1778b5f169ab3d91e5c569630
yasuabe2613 <yasu@yasuabe.net>
2025-04-29 13:20:38 +0900

    solve ex49

---
+++ ex49/main.go
@@ -0,0 +1,129 @@
+// # Ex49: Flickr Photo Search
+//
+// - Take in a search string via GUI.
+// - Fetch Flickr’s public photo feed matching the search.
+// - Display resulting photos visually.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"image"
+	"io"
+	"net/http"
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+	"github.com/nfnt/resize" // Add this import for resizing images
+)
+
+type FlickrResponse struct {
+	Items []struct {
+		Media struct {
+			M string `json:"m"`
+		} `json:"media"`
+	} `json:"items"`
+}
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Flickr Photo Search")
+
+	tagEntry := widget.NewEntry()
+	tagEntry.SetPlaceHolder("Enter tags...")
+	tagEntryContainer := container.New(layout.NewGridWrapLayout(fyne.NewSize(400, tagEntry.MinSize().Height)), tagEntry)
+	statusLabel := widget.NewLabel("Ready")
+	imageContainer := container.NewGridWrap(fyne.NewSize(200, 200)) // Fixed grid size per image
+	scrollableContainer := container.NewScroll(imageContainer)
+	scrollableContainer.SetMinSize(fyne.NewSize(800, 400)) // Fixed scroll area size
+
+	searchButton := widget.NewButton("Search", func() {
+		tags := tagEntry.Text
+		if tags != "" {
+			updateImages(imageContainer, tags, statusLabel)
+		}
+	})
+	toolbar := container.NewHBox(
+		layout.NewSpacer(),
+		widget.NewLabel("Tags:"),
+		tagEntryContainer,
+		searchButton,
+	)
+
+	statusBar := container.NewHBox(statusLabel)
+
+	content := container.NewBorder(toolbar, statusBar, nil, nil, scrollableContainer)
+	myWindow.SetContent(content)
+
+	myWindow.Resize(fyne.NewSize(800, 600))
+	fmt.Println("Starting the application...")
+	myWindow.ShowAndRun()
+}
+
+func updateImages(imageContainer *fyne.Container, tags string, statusLabel *widget.Label) {
+	statusLabel.SetText("Fetching")
+
+	go func() {
+		url := fmt.Sprintf("https://www.flickr.com/services/feeds/photos_public.gne?format=json&tags=%s", tags)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println("Error: Failed to fetch data from Flickr")
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error: Failed to read Flickr response")
+			return
+		}
+
+		var flickrData FlickrResponse
+		jsonStr := strings.TrimPrefix(string(body), "jsonFlickrFeed(")
+		jsonStr = strings.TrimSuffix(jsonStr, ")")
+		if err := json.Unmarshal([]byte(jsonStr), &flickrData); err != nil {
+			fmt.Println("Error: Failed to parse Flickr response")
+			return
+		}
+
+		var images []fyne.CanvasObject
+		for _, item := range flickrData.Items {
+			imgResp, err := http.Get(item.Media.M)
+			if err != nil {
+				continue
+			}
+			func() {
+				defer imgResp.Body.Close()
+
+				img, _, err := image.Decode(imgResp.Body)
+				if err != nil {
+					return
+				}
+
+				resizedImg := resize.Thumbnail(200, 200, img, resize.Lanczos3)
+
+				fyneImg := canvas.NewImageFromImage(resizedImg)
+				fyneImg.SetMinSize(fyne.NewSize(200, 200))
+				fyneImg.FillMode = canvas.ImageFillContain
+				images = append(images, fyneImg)
+			}()
+		}
+
+		// Now back to GUI update
+		// Safe because GUI updates are small and instant
+		imageContainer.Objects = images
+		imageContainer.Refresh()
+
+		if len(images) > 0 {
+			statusLabel.SetText(fmt.Sprintf("Tags: %s", tags))
+		} else {
+			statusLabel.SetText(fmt.Sprintf("No images found for: %s", tags))
+		}
+	}()
+}
--- go.mod
+++ go.mod
@@ -3,25 +3,56 @@ module example.com/mymodule
 go 1.24.2
 
 require (
+	fyne.io/fyne/v2 v2.6.0
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	go.mongodb.org/mongo-driver v1.17.3
 	golang.org/x/exp v0.0.0-20250408133849-7e4ce0ab07d0
 	golang.org/x/net v0.39.0
 )
 
 require (
+	fyne.io/systray v1.11.0 // indirect
+	github.com/BurntSushi/toml v1.5.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fredbi/uri v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/fyne-io/gl-js v0.1.0 // indirect
+	github.com/fyne-io/glfw-js v0.2.0 // indirect
+	github.com/fyne-io/image v0.1.1 // indirect
+	github.com/fyne-io/oksvg v0.1.0 // indirect
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
+	github.com/go-text/render v0.2.0 // indirect
+	github.com/go-text/typesetting v0.2.1 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hack-pad/go-indexeddb v0.3.2 // indirect
+	github.com/hack-pad/safejs v0.1.0 // indirect
+	github.com/jeandeaual/go-locale v0.0.0-20241217141322-fcc2cadd6f08 // indirect
+	github.com/jsummers/gobmp v0.0.0-20230614200233-a9de23ed2e25 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/nicksnyder/go-i18n/v2 v2.5.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rymdport/portal v0.4.1 // indirect
+	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
+	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	github.com/yuin/goldmark v1.7.8 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/image v0.24.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
